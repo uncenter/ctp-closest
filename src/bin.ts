72992d8c2628282ec22d162a26dfc77bd493aa06
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-21 18:24:27 -0500

    style: whitespace

--- src/bin.ts
+++ src/bin.ts
@@ -6,10 +6,8 @@ while (true) {
 	const color = prompt("> ")?.trim();
 	if (!color) continue;
 
-	// @ts-ignore: Missing properties.
 	try {
 		const { input, ...variants } = closest(color);
-
 		console.log(`${green(bold("Input:"))} "${input.hex}"`);
 		console.table(variants);
 	} catch {
